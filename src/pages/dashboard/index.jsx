8f0c2494ded6a1b00e0981d883762dc5f8693302
abel-otegbola <abel15655@gmail.com>
2024-06-14 23:06:28 +0100

    feat: create dashboard routes and addc sideNav

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,14 +1,31 @@
 import { Route, Routes } from 'react-router-dom'
-
 import Overview from './overview/overview'
+import SideNav from '../../components/sideNav/sideNav'
+import SymptomCheck from './symptom-check/symptomCheck'
 
-function App() {
+function Dashboard() {
 
   return (
-      <Routes>
-        <Route path={"/"} exact element={<Overview />} />
-      </Routes>
+    <div className='flex '>
+        <SideNav />
+        <div className="flex-1 p-[3%]">
+          {/* header goes here */}
+
+          <div className="flex flex-wrap justify-between items-start w-full">
+            <div className="md:w-[60%]">
+              <Routes>
+                  <Route path={"/"} exact element={<Overview />} />
+                  <Route path={"/checkup/symptom-check"} exact element={<SymptomCheck />} />
+              </Routes>
+            </div>
+
+            <div className="md:w-[36%] p-4 border border-gray-500/[0.2] rounded-[15px]">
+              helo
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
-export default App
+export default Dashboard
