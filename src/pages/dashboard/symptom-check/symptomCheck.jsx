eaa42bc05783cb33c66422af9a934abef1f11b74
abel-otegbola <abel15655@gmail.com>
2024-06-14 23:06:59 +0100

    feat: create symptomChecker page

---
+++ src/pages/dashboard/symptom-check/symptomCheck.jsx
@@ -0,0 +1,10 @@
+import UserFlow from "../../../components/userFlow/userFlow";
+
+export default function SymptomCheck() {
+    return (
+        <div>
+
+            <UserFlow />
+        </div>
+    )
+}
