abe478b9dec3c0b4b405ac5373ea1f0ab2bf4bc3
abel-otegbola <abel15655@gmail.com>
2024-06-17 00:02:22 +0100

    fix: symptoms component page add and remove symptoms

--- src/components/userFlow/symptoms.jsx
+++ src/components/userFlow/symptoms.jsx
@@ -1,41 +1,52 @@
 import { symptoms } from "../../data/symptoms"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Model from "../../data/model"
+import { FaTimes } from "react-icons/fa"
 
 export default function Symptoms ({ data, setData }) {
-    const [search, setSearch] = useState("")
-    const [symptomsList, setSymptomsList] = useState([...symptoms.head, ...symptoms.chest, ...symptoms.abdomen, ...symptoms.chest, ...symptoms.nose])
+    const [active, setActive] = useState("head")
     
-    const addSymptom = (weight) => {
-        setData({ ...data, weight })
+    const addSymptom = (symptom) => {
+        setData({ ...data, symptoms: data.symptoms?.indexOf(symptom) === -1 ? [...data.symptoms, symptom] : data.symptoms })
     }
 
-    const symptomsArray = [...symptoms.head, ...symptoms.chest, ...symptoms.abdomen, ...symptoms.chest, ...symptoms.nose]
-
-    useEffect(() => {
-        setSymptomsList(symptomsArray.filter(item => item.description.indexOf(search) !== -1))
-    }, [search])
+    const removeSymptom = (id) => {
+        setData({ ...data, symptoms: data.symptoms?.filter(item => item.id !== id) })
+    }
 
     return (
         <div >
-            <div className="flex flex-col rounded-[15px] bg-slate-100/[0.5] border border-gray-500/[0.1] my-4">
-                <p className="flex justify-between font-medium p-4 px-6 border border-transparent border-b-gray-500/[0.1] bg-gray-100 rounded-t-[15px]">
-                    <span>Search Symptoms</span>
+            <div className="flex flex-col rounded-[15px] border border-gray-500/[0.1] my-4">
+                <p className="flex flex-col justify-between font-medium p-4 px-6 border border-transparent border-b-gray-500/[0.1] bg-gray-100 rounded-t-[15px]">
+                    <span>Select Symptoms</span>
+                    <span className="opacity-[0.6] text-[12px] mt-2">Click any part of the model to show symptoms common to the part</span>
                 </p>
                 <div className="gap-4 px-6 py-6 overflow-x-auto">
                     <div className="flex gap-2">
                         <div className="md:w-[36%] w-full p-4 border border-gray-500/[0.2] fill-white stroke-gray-500 rounded-[15px]">
-                        <Model />
+                            <Model active={active} setActive={setActive} />
                         </div>
-                        <div>
+                        <div className="px-4">
+                            <p className="text-center bg-slate-100 rounded p-2 capitalize">{active.replace("_", " ")}</p>
                             <div className="flex flex-wrap text-[12px] gap-2 pt-4 pb-4 my-4 max-h-[250px] overflow-y-auto">
                             {
-                                symptomsList.map((symptom, i) => (
-                                    <button key={i} className="p-2 py-1 rounded border border-gray-500/[0.2]" onClick={() => addSymptom(symptom.description)} title={symptom.description}>{symptom.description}</button>
+                                symptoms[active || "head"]?.map((symptom) => (
+                                    <button key={symptom.id} className="text-start p-2 py-1 rounded border border-gray-500/[0.2]" onClick={() => addSymptom(symptom)} title={symptom.description}>{symptom.description}</button>
                                 ))
                             }
                             </div>
-                            <button className="w-full mb-4 bg-purple text-white border border-gray-500/[0.3] rounded-[10px] p-3" onClick={() => setData({})}>ADD</button>
+
+                            <p>Your Symptoms</p>
+                            <div className="flex flex-wrap text-[12px] gap-2 pt-4 pb-4 my-4 max-h-[250px] overflow-y-auto">
+                            {
+                                data.symptoms?.map((symptom) => (
+                                    <button key={symptom.id} className="flex items-center gap-2 text-start p-2 py-1 rounded border border-gray-500/[0.2]" title={symptom}>
+                                        {symptom.description}
+                                        <FaTimes onClick={() => removeSymptom(symptom.id)}/>
+                                    </button>
+                                ))
+                            }
+                            </div>
                         </div>
                     </div>
                 </div>
