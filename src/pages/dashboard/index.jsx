943a6bd49383746e8d04ad71ce456a4f98ccbeb6
abel-otegbola <abel15655@gmail.com>
2024-06-16 14:43:37 +0100

    style: add full width to dashboard for mobile

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -12,15 +12,14 @@ function Dashboard() {
           {/* header goes here */}
 
           <div className="flex flex-wrap justify-between items-start w-full">
-            <div className="md:w-[60%]">
+            <div className="md:w-[60%] w-full">
               <Routes>
                   <Route path={"/"} exact element={<Overview />} />
                   <Route path={"/checkup/symptom-check"} exact element={<SymptomCheck />} />
               </Routes>
             </div>
 
-            <div className="md:w-[36%] p-4 border border-gray-500/[0.2] rounded-[15px]">
-              helo
+            <div className="md:w-[36%] w-full p-4 border border-gray-500/[0.2] fill-white stroke-gray-500 rounded-[15px]">
             </div>
           </div>
         </div>
