ca7d24365ced57896b9a91bd434144258f448826
abdullahi <moh.abdullahi2003@gmail.com>
2024-06-16 01:28:01 +0100

    completed topbar

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,7 +1,18 @@
+import { FiHome, FiDatabase } from 'react-icons/fi'
+
 export default function Topbar() {
     return (
-        <div>
-            Topbar
-        </div>
+        <header className="flex justify-between items-center py-5 w-[1200px] mx-auto">
+            <h1 className="text-[#807185] text-[18px]">Acad <span className="text-[#AC42C7]">Med</span></h1>
+            <nav className="">
+                <ul className="flex items-center justify-between gap-[60px]">
+
+                    <li className="flex items-center justify-center gap-2"><FiHome /><p>Home</p></li>
+                    <li className="flex items-center justify-center gap-2"><img src="/src/assets/about.png" alt="about image" width="32px" height="32px" /><p>About</p></li>
+                    <li className="flex items-center justify-center gap-2"><FiDatabase /><p>Platform</p></li>
+                </ul>
+            </nav>
+            <img src="/src/assets/profile-pic.png" alt="profile picture" width="42px" height="auto" />
+        </header>
     )
-}
+} 
