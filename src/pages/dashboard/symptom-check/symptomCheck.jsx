1c6783a15578f4b59514c8378db4ddf40cfde5fe
abel-otegbola <abel15655@gmail.com>
2024-06-16 23:58:09 +0100

    feat: add right side in symptom checker page

--- src/pages/dashboard/symptom-check/symptomCheck.jsx
+++ src/pages/dashboard/symptom-check/symptomCheck.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import UserFlow from "../../../components/userFlow/userFlow";
 
 export default function SymptomCheck() {
-    return (
-        <div>
+    const [result, setResult] = useState({})
+    const [data, setData] = useState({ symptoms: [] })
 
-            <UserFlow />
+    return (
+        <div className="flex flex-wrap justify-between">
+            <div className="lg:w-[60%] w-full">
+                <UserFlow result={result} setResult={setResult} data={data} setData={setData}/>
+            </div>
+            
+            <div className="lg:w-[36%] w-full p-4 border border-gray-500/[0.1] fill-white stroke-gray-500 rounded-[15px]">
+                
+            </div>
         </div>
     )
 }
