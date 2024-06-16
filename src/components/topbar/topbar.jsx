562e07809d31ca88ad927258dda015fbace5839e
abel-otegbola <abel15655@gmail.com>
2024-06-16 15:13:57 +0100

    feat: edit topbar height and icon

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,15 +1,14 @@
-import { FiHome, FiDatabase } from 'react-icons/fi'
+import { FiHome, FiDatabase, FiList } from 'react-icons/fi'
 
 export default function Topbar() {
     return (
-        <header className="flex justify-between items-center py-5 max-w-[1200px] mx-auto">
-            <h1 className="text-[#807185] text-[18px]">Acad <span className="text-[#AC42C7]">Med</span></h1>
-            <nav className="">
+        <header className="flex justify-between items-center py-3 px-8 md:pr-8 pr-16">
+            <h1 className="text-[#807185] text-[18px] font-medium">Acad <span className="text-purple">Med</span></h1>
+            <nav className="sm:block hidden">
                 <ul className="flex items-center justify-between gap-[60px]">
-
-                    <li className="flex items-center justify-center gap-2"><a href="/"><FiHome /><p>Home</p></a></li>
-                    <li className="flex items-center justify-center gap-2"><a href="/about"><img src="/src/assets/about.png" alt="about image" width="32px" height="32px" /><p>About</p></a></li>
-                    <li className="flex items-center justify-center gap-2"><a href="/dashboard"><FiDatabase /><p>Platform</p></a></li>
+                    <li><a className="flex items-center justify-center gap-2" href="/"><FiHome /><p>Home</p></a></li>
+                    <li><a className="flex items-center justify-center gap-2" href="/about"><FiList /><p>About</p></a></li>
+                    <li><a className="flex items-center justify-center gap-2" href="/dashboard"><FiDatabase /><p>Platform</p></a></li>
                 </ul>
             </nav>
             <img src="/src/assets/profile-pic.png" alt="profile picture" width="42px" height="auto" />
