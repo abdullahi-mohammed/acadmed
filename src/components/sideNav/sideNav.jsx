f15dc45459984979af0afd819def696a63de647f
abel-otegbola <abel15655@gmail.com>
2024-06-17 15:35:17 +0100

    feat: add login authentication to project

--- src/components/sideNav/sideNav.jsx
+++ src/components/sideNav/sideNav.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { FiDatabase, FiUserCheck, FiCalendar, FiSettings, FiLogOut } from "react-icons/fi"
 import { FaBars, FaTimes } from "react-icons/fa"
 import { Link, useLocation } from "react-router-dom"
 import { useOutsideClick } from "../../customHooks/useClickOutside"
+import { AuthContext } from "../../customHooks/useAuth"
 
 export default function SideNav() {
+    const { logOut } = useContext(AuthContext)
     const pathname = useLocation().pathname
     const [open, setOpen] = useState(false)
 
@@ -18,14 +20,14 @@ export default function SideNav() {
     
     const otherLinks = [
         { id: 0, text: "settings", to: "/dashboard/settings", icon: <FiSettings /> },
-        { id: 1, text: "logout" , to: "", icon: <FiLogOut />},
+        { id: 1, text: "logout" , to: "#", icon: <FiLogOut />},
     ]
 
     return (
         <div 
             ref={sideNavRef}
             className={`w-[250px] px-3 md:h-screen h-full md:bg-[#F7F7F9]/[0.7] dark:bg-black bg-white
-            md:sticky fixed top-0 md:right-auto right-0 border border-[#F7F0FB] transition-all duration-700 z-[4]
+            md:sticky fixed top-[54px] md:right-auto right-0 border border-gray-500/[0.1] transition-all duration-700 md:z-0 z-[4]
             ${open ? "translate-x-[0px]" : "md:translate-x-0 translate-x-[250px]"}
         `}>
 
@@ -43,7 +45,7 @@ export default function SideNav() {
             <p className="text-[#C1B4C7] p-4">GENERAL</p>
             {
                 generalLinks.map(links => ( 
-                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 py-2 hover:bg-purple/[0.2] duration-500 rounded-[8px] ${pathname.indexOf(links.to) !== -1 ? "bg-purple text-white border border-[#F7F0FB]" : ""}`}>
+                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 py-2 duration-500 rounded-[8px] ${pathname.indexOf(links.to) !== -1 ? "bg-purple text-white" : "hover:bg-purple/[0.2] "}`}>
                         <span className="text-[17px]">{links.icon}</span>
                         {links.text}
                     </Link>
@@ -53,7 +55,7 @@ export default function SideNav() {
             <p className="text-[#C1B4C7] p-4">OTHERS</p>
             {
                 otherLinks.map(links => ( 
-                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 py-2 hover:bg-purple/[0.2] duration-500 rounded-[8px] ${pathname === links.to ? "bg-purple text-white border border-[#F7F0FB]" : ""}`}>
+                    <Link key={links.id} to={links.to} onClick={() => links.text === "logout" ? logOut() : ""} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 py-2 duration-500 rounded-[8px] ${pathname === links.to ? "bg-purple text-white" : "hover:bg-purple/[0.2] "}`}>
                         <span className="text-[17px]">{links.icon}</span>
                         {links.text}
                     </Link>
--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,11 +1,22 @@
+import { useContext, useEffect } from 'react'
 import { FiHome, FiDatabase, FiList } from 'react-icons/fi'
 import { Link, useLocation } from 'react-router-dom'
+import { AuthContext } from '../../customHooks/useAuth'
 
 export default function Topbar() {
     const pathname = useLocation().pathname
+    const {user} = useContext(AuthContext)
+
+    useEffect(() =>{
+        if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark')
+        } else {
+            document.documentElement.classList.remove('dark')
+        }
+    })
 
     return (
-        <header className="flex justify-between items-center sticky top-0 shadow-sm bg-white py-2 px-8 md:pr-8 pr-16 z-[3]">
+        <header className="flex justify-between items-center sticky top-0 shadow-sm bg-white dark:bg-black py-2 md:px-8 px-4 z-[3]">
             <Link to={"/"} className="text-[18px] font-medium">Acad<span className="text-purple">Med</span></Link>
             <nav className="sm:block hidden">
                 <ul className="flex items-center justify-between gap-4 font-medium">
@@ -14,7 +25,18 @@ export default function Topbar() {
                     <li><a className={`flex items-center justify-center px-4 py-2 rounded gap-2 ${pathname.split("/").indexOf("dashboard") !== -1 ? "bg-purple/[0.1] text-purple" : ""}`} href="/dashboard"><FiDatabase /><p>Platform</p></a></li>
                 </ul>
             </nav>
-            <img src="/images/profile-pic.png" alt="profile picture" width="42px" height="auto" />
+            {
+                user ? 
+                <Link
+                    to="/dashboard"
+                    className="flex items-center justify-center w-[35px] h-[35px] py-0 rounded-full bg-slate-300/[0.5] hover:outline outline-offset-2 outline-purple/[0.1] hover:text-darkpurple"
+                    role="menuitem" 
+                >
+                    { user?.photoURL ? <img src={user?.photoURL} alt="user" className="rounded-full" width={35} height={35} /> : user?.email.charAt(0)}
+                </Link>
+                :
+                <a href="/login" className="md:block hidden px-6 py-2 bg-black text-white rounded">Login</a>
+            }
         </header>
     )
 } 
