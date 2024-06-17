8921086212d3f8bf7e866a5ec3ff6a8e0999623c
abel-otegbola <abel15655@gmail.com>
2024-06-17 09:15:45 +0100

    refactor: remove excess codes in selectField and sideNav

--- src/components/selectField/selectField.jsx
+++ src/components/selectField/selectField.jsx
@@ -6,8 +6,15 @@ function SelectField({ text, action, value, options }) {
     return (
         <div className="w-full py-4 text-[12px]">
             <div className={`relative border p-[2px] rounded ${focus ? "outline-1 outline-purple/[0.2] outline-offset-1 border-purple" : " border-gray-500/[0.2]"}`}>
-                <select id={text} className="p-2 border-none outline-none bg-transparent w-full" defaultValue={value} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} onChange={(e) => {action(text, e.target.value)}}>
-                    <option >Select one</option>
+                <select 
+                    id={text} 
+                    className="p-2 border-none outline-none bg-transparent w-full" 
+                    defaultValue={value} 
+                    onFocus={() => setFocus(true)} 
+                    onBlur={() => setFocus(false)} 
+                    onChange={(e) => {action(text, e.target.value)}}
+                >
+                    <option className="opacity-[0.5]">Select one</option>
                     {
                         options.map(option => (
                             <option value={option.value} key={option.id}>{option.label || option.value}</option>
--- src/components/sideNav/sideNav.jsx
+++ src/components/sideNav/sideNav.jsx
@@ -2,10 +2,13 @@ import { useState } from "react"
 import { FiDatabase, FiUserCheck, FiCalendar, FiSettings, FiLogOut } from "react-icons/fi"
 import { FaBars, FaTimes } from "react-icons/fa"
 import { Link, useLocation } from "react-router-dom"
+import { useOutsideClick } from "../../customHooks/useClickOutside"
 
 export default function SideNav() {
     const pathname = useLocation().pathname
     const [open, setOpen] = useState(false)
+
+    const sideNavRef = useOutsideClick(setOpen)
 
     const generalLinks = [
         { id: 0, text: "dashboard", to: "/dashboard", icon: <FiDatabase /> },
@@ -20,11 +23,13 @@ export default function SideNav() {
 
     return (
         <div 
+            ref={sideNavRef}
             className={`w-[250px] px-3 md:h-screen h-full md:bg-[#F7F7F9]/[0.7] dark:bg-black bg-white
             md:sticky fixed top-0 md:right-auto right-0 border border-[#F7F0FB] transition-all duration-700 z-[4]
             ${open ? "translate-x-[0px]" : "md:translate-x-0 translate-x-[250px]"}
         `}>
 
+            {/* open and close button  */}
             <button 
                 className={`p-4 py-6 ml-auto md:hidden relative bg-white
                 transition-all duration-700
@@ -38,7 +43,7 @@ export default function SideNav() {
             <p className="text-[#C1B4C7] p-4">GENERAL</p>
             {
                 generalLinks.map(links => ( 
-                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 hover:bg-purple/[0.2] duration-500 rounded-[15px] ${pathname === links.to ? "bg-purple text-white border border-[#F7F0FB]" : ""}`}>
+                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 py-2 hover:bg-purple/[0.2] duration-500 rounded-[8px] ${pathname === links.to ? "bg-purple text-white border border-[#F7F0FB]" : ""}`}>
                         <span className="text-[17px]">{links.icon}</span>
                         {links.text}
                     </Link>
@@ -48,7 +53,7 @@ export default function SideNav() {
             <p className="text-[#C1B4C7] p-4">OTHERS</p>
             {
                 otherLinks.map(links => ( 
-                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 hover:bg-purple/[0.2] duration-500 rounded-[15px] ${pathname === links.to ? "bg-purple text-white border border-[#F7F0FB]" : ""}`}>
+                    <Link key={links.id} to={links.to} className={`flex mb-1 mx-1 capitalize items-center gap-4 p-4 py-2 hover:bg-purple/[0.2] duration-500 rounded-[8px] ${pathname === links.to ? "bg-purple text-white border border-[#F7F0FB]" : ""}`}>
                         <span className="text-[17px]">{links.icon}</span>
                         {links.text}
                     </Link>
