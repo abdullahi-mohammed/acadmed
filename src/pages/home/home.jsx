b5cee50734986faf8575234d860758c7562d52c2
abel-otegbola <abel15655@gmail.com>
2024-06-17 09:10:15 +0100

    feat: add optimized imgs and responsive for home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,17 +1,17 @@
 export default function Home() {
     return (
-        <div className="bg-[url(/src/assets/hero-bg.png)]">
-            <div className="flex justify-between items-center w-[1200px] mx-auto my-auto min-h-screen">
-                <div className="flex justify-between flex-col gap-7 text-[#906EA0]">
-                    <h2 className="text-[40px] leading-[130%] text-[#422862] font-semibold w-[80%] text-balance">
+        <div className="bg-[url(/images/hero-bg.webp)] bg-cover">
+            <div className="flex lg:flex-nowrap flex-wrap px-8 justify-between items-center min-h-[600px]">
+                <div className="flex justify-center lg:w-[40%] w-full lg:items-start items-center lg:text-start text-center flex-col gap-7 text-[#906EA0] min-h-[60vh]">
+                    <h2 className="md:text-[40px] text-[32px] leading-[130%] text-[#422862] font-semibold text-balance">
                         Intelligent Telemedicine Assessment and Simple Schedule planner
                     </h2>
-                    <p className="w-[74%]">We are pioneering the future of healthcare for academias with cutting edge telehealth and telemedicine solutions.</p>
+                    <p className="">We are pioneering the future of healthcare for academias with cutting edge telehealth and telemedicine solutions.</p>
                     <p>AI Technology</p>
-                    <a href="#" className="text-white bg-[#2A3443] px-3 py-1 w-fit rounded">Get Started</a>
+                    <a href="#" className="block text-white bg-[#2A3443] px-8 py-3 sm:w-fit w-full rounded">Get Started</a>
                 </div>
 
-                <img src="/src/assets/hero-image.png" alt="hero image" width="600px" />
+                <img src="/images/hero-image.webp" alt="hero image" className="md:w-[600px] w-[90%] mx-auto" />
             </div>
         </div>
     )
