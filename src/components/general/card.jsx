4b32bb7ef2b0436989129c78e14b0d38231226fb
abel-otegbola <abel15655@gmail.com>
2024-06-18 11:24:25 +0100

    feat: add checkup data functionalities

--- src/components/general/card.jsx
+++ src/components/general/card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = (props) => {
     return (
-        <div className='bg-[#F7F0FB]'>
+        <div className='bg-[#F7F0FB] dark:bg-gray-400/[0.1] p-4 '>
             {props.children}
         </div>
     )
