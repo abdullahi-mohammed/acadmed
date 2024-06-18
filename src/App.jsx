3bddb3c7ebec94d923828d33ee7ec646a07dded0
abel-otegbola <abel15655@gmail.com>
2024-06-18 09:32:46 +0100

    feat: add schedule provider context

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,22 @@ import Topbar from './components/topbar/topbar'
 import Dashboard from './pages/dashboard'
 import Login from './pages/login/login'
 import AuthProvider from './customHooks/useAuth'
+import SchedulesProvider from './context/scheduleContext'
 
 function App() {
 
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <Topbar />
-        <Routes>
-          <Route path={"/"} exact element={<Home />} />
-          <Route path={"/login"} exact element={<Login />} />
-          <Route path={"/dashboard/*"} exact element={<Dashboard />} />
-        </Routes>
-      </BrowserRouter>
+      <SchedulesProvider>
+        <BrowserRouter>
+          <Topbar />
+          <Routes>
+            <Route path={"/"} exact element={<Home />} />
+            <Route path={"/login"} exact element={<Login />} />
+            <Route path={"/dashboard/*"} exact element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </SchedulesProvider>
     </AuthProvider>
   )
 }
