e25057a0f6a5fe9a71105c0b07bc5a1cd4097e9e
abel-otegbola <abel15655@gmail.com>
2024-07-05 13:30:45 +0100

    feat: add taskTypes in overview [x] add General, Study, Checkup, Fitness task cards

--- src/pages/dashboard/overview/overview.jsx
+++ src/pages/dashboard/overview/overview.jsx
@@ -1,12 +1,13 @@
-import { useContext, useEffect } from "react"
+import { useContext } from "react"
 import { AuthContext } from "../../../customHooks/useAuth"
 import GeneralHeader from "../../../components/general/header"
 import { CheckupContext } from "../../../context/checkupContext"
-import { PiBackpack, PiCalendar } from "react-icons/pi"
+import { PiAtom, PiBackpack, PiBook, PiCalendar, PiHand, PiStethoscope } from "react-icons/pi"
 import { Link } from "react-router-dom"
 import BotTips from "../../../components/botTips/botTips"
 import ScheduleGrid from "../../../components/scheduleGrid/scheduleGrid"
 import { SchedulesContext } from "../../../context/scheduleContext"
+import TaskType from "../../../components/taskType/taskType"
 
 
 export default function Overview() {
@@ -15,56 +16,45 @@ export default function Overview() {
   const { schedules } = useContext(SchedulesContext)
   const checkup = checkups.filter(item => item.user === user.email)[0]
 
-  useEffect(() => {
-    console.log(checkup)
-  }, [])
-
   return (
     <div>
       <GeneralHeader h2={`Welcome, ${user.displayName || user.email.split("@")[0] }`} p={"Insights to your checkup history and schedules planning"} />
 
       
+      <div className="py-2 grid lg:grid-cols-4 md:gris-cols-3 grid-cols-2 gap-2">
+        <TaskType title={"General"} icon={<PiAtom />} schedules={schedules}/>
+        <TaskType title={"Study"} icon={<PiBook />} schedules={schedules}/>
+        <TaskType title={"Checkup"} icon={<PiStethoscope />} schedules={schedules}/>
+        <TaskType title={"Fitness"} icon={<PiHand />} schedules={schedules}/>
+      </div>
+
       <div className="flex gap-2 flex-wrap items-start justify-between py-4">
+
         
-        <div className='md:w-[70%] w-full flex-1 flex flex-col justify-between items-start bg-[#FFFFFF] dark:bg-black border-gray-500/[0.2] border p-6 rounded-[10px]'>
+        <div className='md:w-[70%] w-full flex-1 flex flex-col justify-between items-start bg-[#FFFFFF] dark:bg-black border-gray-500/[0.2] border p-6 rounded-[10px]'>      
+
           <div className="w-full">
               <h3 className="font-semibold border border-transparent border-b-gray-500/[0.2] pb-4">Checkup Results</h3>
           </div>
+                  
+                  {
+                      checkup?.conditions?.map(condition => (
+                              <div 
+                                key={condition.id}
+                                className={`flex flex-col p-4 gap-2 rounded-[10px] my-2 border w-full
+                                ${condition.probability > 0.5 ? "bg-green-400/[0.09] border-green-400" : 
+                                    condition.probability > 0.3 ? "bg-yellow-400/[0.09] border-orange-400/[0.4]" 
+                                    : "bg-gray-400/[0.1] border-gray-500/[0.1]"}`} 
+                            >
+                                <p className="font-medium">{condition.name}</p>
+                                <div className="flex gap-1 text-[10px] opacity-[0.6]">
+                                    <p>Probability: </p>
+                                    <p>{condition.probability}</p>
+                                </div>
+                            </div>
+                      ))
+                  }
 
-          <table className="table-auto w-full">
-            <thead>
-                <tr className="text-[12px] p-4">
-                    
-                </tr>
-            </thead>
-            <tbody className="text-[10px]">
-                {
-                    [...Array(checkup?.conditions?.length).keys()].map(date => (
-                    <tr key={date} className="h-[65px] border border-transparent border-y-gray-500/[0.2]">
-                        
-                        {
-                            checkup?.conditions?.map(condition => (
-                                    <td 
-                                      key={condition.id}
-                                      className={`flex flex-col p-4 gap-2 rounded-[10px] my-2 border
-                                      ${condition.probability > 0.5 ? "bg-green-400/[0.09] border-green-400" : 
-                                          condition.probability > 0.3 ? "bg-yellow-400/[0.09] border-orange-400/[0.4]" 
-                                          : "bg-gray-400/[0.1] border-gray-500/[0.1]"}`} 
-                                  >
-                                      <p className="font-medium">{condition.name}</p>
-                                      <div className="flex gap-1 text-[10px] opacity-[0.6]">
-                                          <p>Probability: </p>
-                                          <p>{condition.probability}</p>
-                                      </div>
-                                  </td>
-                            ))
-                        }
-                    </tr> 
-                    ))
-                }
-            </tbody>
-
-          </table>
           <Link to={"/dashboard/checkup"} className="flex items-center gap-2 px-6 py-2 mt-4 bg-purple text-white rounded">View Checkup History  <PiBackpack /></Link>
 
           <div className='my-4 w-full flex-1 flex flex-col justify-between items-start bg-[#FFFFFF] dark:bg-black border-gray-500/[0.2] border p-6 rounded-[10px]'>
